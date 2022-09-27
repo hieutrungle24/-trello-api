39763d8edc29556c147d867718af9f2b207bedf5
hieutrungle24 <hieucalm77@gmail.com>
2022-09-27 16:42:29 +0700

    Initial prọect NodeJs

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express()
+
+const hostname = 'localhost'
+const port = 8088
+
+app.get('/', (req,res) => {
+    res.end('<h1>Hola world</h1>')
+})
+app.listen(port, hostname, () => {
+    console.log(`Server is running at ${hostname}:${port}/`)
+})
